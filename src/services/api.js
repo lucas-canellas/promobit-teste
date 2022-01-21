a4865c0f88a90b289a35b86c1e364589f9edc6ef
lucas-canellas <lucascanellas91@gmail.com>
2022-01-21 11:49:03 -0300

    API KEY

--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,7 @@ import axios from "axios";
 
 
 
-// const API_KEY = process.env.API_KEY;
-// const API_KEY = d680466ec4a78aa53efd51a42b703f2c;
-
+const API_KEY = process.env.API_KEY;
 
 const api = axios.create({
   baseURL:
@@ -15,7 +13,7 @@ const api = axios.create({
 api.interceptors.request.use(config => {
   config.params = {
     // add your default ones
-    api_key: `d680466ec4a78aa53efd51a42b703f2c`,
+    api_key: `${API_KEY}`,
     language: 'pt-BR',
     // spread the request's params
     ...config.params,
