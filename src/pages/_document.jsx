91a837b7f2dc3da775c3850aa9d34e766e5afa07
Lucas Canellas <lucascanellas91@gmail.com>
2022-03-15 19:08:28 -0300

    FOUC

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,25 +1,26 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
+class MyDocument extends Document {
+	static async getInitialProps(ctx) {
+		const initialProps = await Document.getInitialProps(ctx)
+		return { ...initialProps }
+	}
 
-  
-    return (
-      <Html>
-        <Head>
+	render() {
+		return (
+			<Html>
+				<Head>
           <link rel="icon" type="image/png" href="https://img.icons8.com/color/48/000000/film-noir.png" />
           <link href="https://fonts.googleapis.com/css2?family=Martel+Sans:wght@200;300;400;600;700;800;900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
         </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+				<body>
+					<script>0</script>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		)
+	}
 }
 
 export default MyDocument
