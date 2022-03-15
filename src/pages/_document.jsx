ff692ceed6caaea22507dcf995db6f660f7dea30
Lucas Canellas <lucascanellas91@gmail.com>
2022-03-15 19:12:29 -0300

    att

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,26 +1,38 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-	static async getInitialProps(ctx) {
-		const initialProps = await Document.getInitialProps(ctx)
-		return { ...initialProps }
-	}
+  static async getInitialProps(ctx) {
+    const originalRenderPage = ctx.renderPage
 
-	render() {
-		return (
-			<Html>
-				<Head>
+    // Run the React rendering logic synchronously
+    ctx.renderPage = () =>
+      originalRenderPage({
+        // Useful for wrapping the whole react tree
+        enhanceApp: (App) => App,
+        // Useful for wrapping in a per-page basis
+        enhanceComponent: (Component) => Component,
+      })
+
+    // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
+
+  render() {
+    return (
+      <Html>
+        				<Head>
           <link rel="icon" type="image/png" href="https://img.icons8.com/color/48/000000/film-noir.png" />
           <link href="https://fonts.googleapis.com/css2?family=Martel+Sans:wght@200;300;400;600;700;800;900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
         </Head>
-				<body>
-					<script>0</script>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		)
-	}
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
 
 export default MyDocument
